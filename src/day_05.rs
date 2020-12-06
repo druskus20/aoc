29b8f56e219be525fbf948ea690c0e40fcdbef33
druskus20 <druskus20@gmail.com>
2020-12-06 04:04:14 +0100

    aoc 2020 day 5 part 2

--- src/day_05.rs
+++ src/day_05.rs
@@ -1,5 +1,7 @@
 use anyhow::*;
 use std::ops::RangeInclusive;
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
 #[aoc_generator(day5)]
 pub fn input_gen(input: &str) -> Vec<String> {
@@ -11,11 +13,11 @@ fn split_range(range: &RangeInclusive<usize>) -> usize {
 }
 
 fn get_last_half(range: &RangeInclusive<usize>) -> RangeInclusive<usize> {
-    *range.start() + half_range(range)..=*range.end()
+    *range.start() + split_range(range)..=*range.end()
 }
 
 fn get_first_half(range: &RangeInclusive<usize>) -> RangeInclusive<usize> {
-    *range.start()..=*range.end() - half_range(range)
+    *range.start()..=*range.end() - split_range(range)
 }
 
 pub fn get_half_rows(code: char, range: &RangeInclusive<usize>) -> Result<RangeInclusive<usize>> {
@@ -48,10 +50,47 @@ pub fn find_row_col(code: &String) -> (usize, usize) {
     (*row_range.start(), *col_range.end())
 }
 
-#[aoc(day5, part1)]
-pub fn solve_part1(data: &Vec<String>) -> usize {
-    let row_cols = data.iter().map(find_row_col).collect::<Vec<_>>();
-    row_cols.iter().map(|p| p.0 * 8 + p.1).max().unwrap()
+pub fn  data_to_numeric(data: &Vec<String>) -> Vec<usize> {
+    data.iter().map(find_row_col).map(|p| p.0 * 8 + p.1).collect::<Vec<_>>()
 }
 
-//#[aoc(day5, part2)]
+#[aoc(day5, part1)]
+pub fn solve_part1(data: &Vec<String>) -> usize {
+    *data_to_numeric(data).iter().max().unwrap()
+}
+
+pub fn merge_seats(data: &Vec<usize>) -> Vec<usize> {
+    let half = ((data.len() / 2) as f32).round() as usize;
+
+    let mut merged_vector = Vec::<usize>::new();
+
+    let lower_data = data[half..].to_vec();
+    let mut higher_data = data[..half].to_vec();
+    higher_data.reverse();
+
+    lower_data
+        .iter()
+        .zip(higher_data.iter())
+        .for_each(|(low_val, high_val)| merged_vector.append(&mut vec!(*low_val, *high_val)));
+
+    merged_vector
+}
+
+#[aoc(day5, part2)]
+pub fn solve_part2(data: &Vec<String>) -> usize {
+
+    let num_data = data_to_numeric(data);
+
+    // Create an array of all possible seats within the range
+    let possible_seats = (*num_data.iter().min().unwrap()..*num_data.iter().max().unwrap()).collect::<Vec<usize>>();
+
+    // Create a hash of the present seats
+    let num_data_hash : HashSet<usize> = HashSet::from_iter(num_data);
+
+    // Merge the order of the possible seats
+    let merged_possible_seats = merge_seats(&possible_seats);
+
+    // Find the first missing seat (starting from the middle of the plane)
+    *merged_possible_seats.iter().find(|v| !num_data_hash.contains(v)).unwrap()
+}
+
