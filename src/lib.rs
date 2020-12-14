8576caf6ecd3d8fc547e04acde16a3f5cf534231
druskus20 <druskus20@gmail.com>
2020-12-14 22:47:33 +0100

    aoc 2020 day 7

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,15 @@ extern crate aoc_runner;
 
 #[macro_use]
 extern crate aoc_runner_derive;
+#[macro_use]
+extern crate lazy_static;
 
-pub mod day_01;
-pub mod day_02;
-pub mod day_03;
-pub mod day_04;
+mod day_01;
+mod day_02;
+mod day_03;
+mod day_04;
 mod day_05;
 mod day_06;
+mod day_07;
 
 aoc_lib! { year = 2020 }
